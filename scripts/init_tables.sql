f966cc85d6aa11239ed900e05faea44dc548deb6
Thijs Wassing <Thijswassing@me.com>
2025-09-30 12:49:57 +0200

    Update init_tables.sql

--- scripts/init_tables.sql
+++ scripts/init_tables.sql
@@ -58,7 +58,8 @@ IF OBJECT_ID ('Bronze.erp_cust_az12', 'U') IS NOT NULL
 
 CREATE TABLE Bronze.erp_cust_az12(
 	cid NVARCHAR(50),
-	cntry NVARCHAR(50)
+	bdate DATE,
+	gen NVARCHAR(50)
 );
 
 IF OBJECT_ID ('Bronze.erp_loc_a101', 'U') IS NOT NULL
@@ -66,8 +67,7 @@ IF OBJECT_ID ('Bronze.erp_loc_a101', 'U') IS NOT NULL
 
 CREATE TABLE Bronze.erp_loc_a101(
 	cid NVARCHAR(50),
-	bdate DATE,
-	gen NVARCHAR(50)
+	cntry NVARCHAR(50)
 );
 
 IF OBJECT_ID ('Bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
